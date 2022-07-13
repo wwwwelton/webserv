93158ee6ab798418c75fa940027b75ab72dcbdc6
VLN37 <jofelipe@student.42sp.org.br>
2022-07-13 06:24:06 -0300

    refactor: webserv transformed into class

--- sources/webserv.cpp
+++ sources/webserv.cpp
@@ -1,83 +1,78 @@
 // Copyright (c) 2022 João Rodriguez A.K.A. VLN37. All rights reserved.
 // Creation date: 21/06/2022
 
-#include "webserv.h"
+#include "webserv.hpp"
 #include "Logger.hpp"
 
-void accept_connections(std::vector<_pollfd> *pollfds,
-                        std::vector<req> *clientlist,
-                        Server *host) {
+WebServ::WebServ(int argc, char **argv) {
+  size_t i;
+  Config configs;
+  (void)argc;
+
+  conn = 0;
+  compress = false;
+  clientlist.reserve(1024);
+  clientlist.resize(1024);
+  std::cout << clientlist.size() << "\n";
+
+  configs = Config(argv);
+
+  for (i = 0; i < configs.size(); i++) {
+    configs[i]->_connect(configs.backlog);
+
+    serverlist.insert(std::make_pair(configs[i]->sockfd, configs[i]));
+    pollfds.push_back(_pollfd(configs[i]->sockfd, POLLIN));
+  }
+}
+
+int WebServ::_poll(void) {
+  conn = poll((struct pollfd *)&(*pollfds.begin()), pollfds.size(), -1);
+  // logger.debug() << "returned connections: " << conn << '\n';
+  return conn;
+}
+
+void WebServ::_accept(int i) {
+  Server *host = serverlist[pollfds[i].fd];
   int new_sd;
 
   new_sd = accept(host->sockfd, NULL, NULL);
   while (new_sd != -1) {
-    (*clientlist)[new_sd].request = new Request(new_sd);
-    (*clientlist)[new_sd].server = host;
-    pollfds->push_back(_pollfd(new_sd, POLLIN));
+    clientlist[new_sd].request = new Request(new_sd);
+    clientlist[new_sd].server = host;
+    pollfds.push_back(_pollfd(new_sd, POLLIN));
     new_sd = accept(host->sockfd, NULL, NULL);
   }
 }
 
-void send_messages(int fd, std::vector<req> *clientlist) {
+void WebServ::_respond(int i) {
+  int fd = pollfds[i].fd;
   RequestHandler req_handler;
   Request *ptr;
   Response res;
 
   //   req = Request(fd, clientlist[fd]);
-  ptr = (*clientlist)[fd].request->receive();
-  req_handler = RequestHandler(*ptr, (*clientlist)[fd].server);
+  ptr = clientlist[fd].request->receive(fd);
+  req_handler = RequestHandler(*ptr, clientlist[fd].server);
   req_handler.process();
   res = req_handler._response();
   res._send(fd);
-  delete (*clientlist)[fd].request;
-  (*clientlist)[fd].request = NULL;
-  (*clientlist)[fd].server = NULL;
+  delete clientlist[fd].request;
+  clientlist[fd].request = NULL;
+  clientlist[fd].server = NULL;
+  close(pollfds[i].fd);
+  pollfds[i].fd = -1;
+  compress = true;
 }
 
-void compress_array(std::vector<_pollfd> *pollfds) {
-  std::vector<_pollfd>::iterator it = pollfds->begin();
-  std::vector<_pollfd>::iterator ite = pollfds->end();
+void WebServ::purge_conns(void) {
+  std::vector<_pollfd>::iterator it = pollfds.begin();
+  std::vector<_pollfd>::iterator ite = pollfds.end();
   for (; it != ite; it++) {
-    if (it->fd == -1)
-      pollfds->erase(it);
-  }
-}
-
-int main(int argc, char **argv) {
-  std::map<int, Server *> serverlist;
-  std::vector<req>        clientlist;
-  std::vector<_pollfd>    pollfds;
-  int conn, compress = false;
-  Logger logger(LVL_DEBUG);
-
-  clientlist.reserve(1024);
-  init(argc, argv, &serverlist, &pollfds);
-  logger.debug() << "initializing webserv" << std::endl;
-
-  while (1) {
-    conn = poll((struct pollfd *)&(*pollfds.begin()), pollfds.size(), -1);
-    logger.debug() << "returned connections: " << conn << '\n';
-    if (conn <= 0)
-      break;
-    for (int i = 0, size = pollfds.size(); i < size; i++) {
-      if (pollfds[i].revents == 0) {
-        continue;
-      }
-      logger.info() << "events detected on socket " << pollfds[i].fd << std::endl;
-      if (serverlist[pollfds[i].fd]) {
-        accept_connections(&pollfds, &clientlist, serverlist[pollfds[i].fd]);
-      } else {
-        send_messages(pollfds[i].fd, &clientlist);
-        close(pollfds[i].fd);
-        pollfds[i].fd = -1;
-        compress = true;
-      }
-    }
-    if (compress) {
-      compress = false;
-      compress_array(&pollfds);
+    if (it->fd == -1) {
+      pollfds.erase(it);
+      ite--;
+      if (it == ite)
+        break;
     }
   }
-  for (size_t i = 0; i < pollfds.size(); i++)
-    close(pollfds[i].fd);
 }
---
+++ sources/webserv.hpp
@@ -0,0 +1,64 @@
+// Copyright (c) 2022 João Rodriguez A.K.A. VLN37. All rights reserved.
+// Creation date: 21/06/2022
+
+#ifndef WEBSERV_HPP
+#define WEBSERV_HPP
+
+#define PORT1 3492
+#define PORT2 3493
+
+#include <fcntl.h>
+#include <netdb.h>
+#include <poll.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <cerrno>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <set>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "HttpBase.hpp"
+#include "HttpHandler.hpp"
+#include "HttpRequest.hpp"
+#include "Pollfd.hpp"
+#include "Server.hpp"
+#include "Config.hpp"
+#include "defines.hpp"
+
+class Server;
+typedef struct addrinfo s_addrinfo;
+
+typedef struct s_request {
+  s_request()
+    : server(NULL), request(NULL) { }
+  s_request(Server *_server, int fd)
+    : server(_server), request(new Request(fd)) { }
+  Server  *server;
+  Request *request;
+} req;
+
+class WebServ {
+public:
+  WebServ(int argc, char **argv);
+  int _poll(void);
+  void _accept(int fd);
+  void _respond(int fd);
+  void purge_conns(void);
+
+public:
+  std::map<int, Server *> serverlist;
+  std::vector<req>        clientlist;
+  std::vector<_pollfd>    pollfds;
+  int                     conn;
+  int                     compress;
+};
+
+#endif  // WEBSERV_HPP
