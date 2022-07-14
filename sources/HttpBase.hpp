b293a19d3b578030e08ae8e86d9030b253e0ed87
VLN37 <jofelipe@student.42sp.org.br>
2022-07-13 22:49:44 -0300

    chore: copy notice changes to include intra login

--- sources/HttpBase.hpp
+++ sources/HttpBase.hpp
@@ -1,8 +1,8 @@
 //##############################################################################
 //#              Copyright(c)2022 Turbo Development Design (TDD)               #
-//#                           João Rodriguez                                   #
-//#                            Paulo Sergio                                    #
-//#                            Welton Leite                                    #
+//#                         João Rodriguez - VLN37                             #
+//#                         Paulo Sergio - psergio-                            #
+//#                         Welton Leite - wleite                              #
 //##############################################################################
 
 #ifndef HTTPBASE_HPP
