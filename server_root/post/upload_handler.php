2616bbb91430c35e227240a8615da8dfa20818fa
Paulo Santana <paulo.santana.r@gmail.com>
2022-07-20 20:12:22 -0300

    feat: improved upload examples

---
+++ server_root/post/upload_handler.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Upload</title>
+    </head>
+    <body>
+        <pre>
+        <?php
+            var_dump($_FILES);
+
+        ?>
+        </pre>
+        <p>echo "Hello, world!";</p>
+    </body>
+</html>
