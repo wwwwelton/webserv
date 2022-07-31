d7b93f91c25d4bc0ffe2fc58d555eb40d6ad9c98
Paulo Santana <paulo.santana.r@gmail.com>
2022-07-31 17:18:14 -0300

    # On branch post_cgi # Your branch is up to date with 'origin/post_cgi'. # # Changes to be committed: #   (use "git restore --staged <file>..." to unstage) # 	modified:   server_root/sito/upload.php # # Untracked files: #   (use "git add <file>..." to include in what will be committed) # 	server_root/sito/uploads/ # 	test.conf

--- server_root/sito/upload.php
+++ server_root/sito/upload.php
@@ -10,14 +10,18 @@ var_dump($_FILES);
 //       echo "<tr><td>" . $key . ":</td><td>" . $value . "</td><tr><br/>";
 //    }
 
+
+$target_dir = "uploads/";
+mkdir($target_dir, 0777);
+
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+
 if(file_exists($target_file)) {
     chmod($target_file,0755); //Change the file permissions if allowed
     unlink($target_file); //remove the file
-	echo("\n" . $target_file . "deleted.\n");
+	echo("\n" . $target_file . " deleted.\n");
 }
 
-$target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
