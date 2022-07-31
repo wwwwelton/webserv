397babe25198d05e32b89af48a1febc1b7698b65
Paulo Santana <paulo.santana.r@gmail.com>
2022-07-31 14:41:23 -0300

    chore: merge origin

--- server_root/post/upload_handler.php
+++ server_root/post/upload_handler.php
@@ -1,29 +1,33 @@
-<?php
-    // echo "\nSERVER\n";
-    // var_dump($_SERVER);
-    // echo "\nENV\n";
-    // var_dump($_ENV);
-    // echo "\nSESSION\n";
-    // var_dump($_SESSION);
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Upload</title>
+    </head>
+    <body>
+        <pre>
+        <?php
+            echo "raw: "; var_dump($HTTP_RAW_POST_DATA);
+            echo "files: "; var_dump($_FILES);
+            echo "post: "; var_dump($_POST);
+            echo "server: "; var_dump($_SERVER);
+            echo "request: "; var_dump($_REQUEST);
 
-    echo "\nRAW\n";
-    var_dump($HTTP_RAW_POST_DATA);
-    echo "\nFILES\n";
-    var_dump($_FILES);
-    echo "\nPOST\n";
-    var_dump($_POST);
+            // echo "\nRAW\n";
+            // var_dump($HTTP_RAW_POST_DATA);
+            // echo "\nFILES\n";
+            // var_dump($_FILES);
+            // echo "\nPOST\n";
+            // var_dump($_POST);
+            //
+            // $f = fopen( 'php://stdin', 'r' );
+            //
+            // while( $line = fgets( $f ) ) {
+            //   echo "line: " .$line;
+            // }
+            //
+            // fclose( $f );
 
-    $f = fopen( 'php://stdin', 'r' );
-
-    while( $line = fgets( $f ) ) {
-      echo "line: " .$line;
-    }
-
-    fclose( $f );
-
-    // // echo $_ENV["CONTENT_LENGTH"];
-    // echo $_SERVER["CONTENT_LENGTH"];
-    // echo getenv("CONTENT_LENGTH");
-    // echo $_FILES[0][0];
-    // echo htmlspecialchars($_POST["e-mail"]);
-?>
+        ?>
+        </pre>
+    </body>
+</html>
