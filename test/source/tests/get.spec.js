c741f3284d382619c2a1c0d051b99b4dffd6b28c
wleite <wleite@student.42sp.org.br>
2022-08-10 19:24:41 -0300

    test: add more get index tests

--- test/source/tests/get.spec.js
+++ test/source/tests/get.spec.js
@@ -3,6 +3,10 @@ const request = require('supertest');
 let server1 = "http://127.0.0.1:3490";
 
 describe("GET", () => {
+	// beforeEach(async () => {
+	// 	await new Promise(r => setTimeout(() => r(), 1000));
+	// });
+
 	test(server1 + " should return 200", async () => {
 		const response = await request(server1)
 			.get("");
@@ -18,6 +22,18 @@ describe("GET", () => {
 	test(server1 + "/index.html should return 200", async () => {
 		const response = await request(server1)
 			.get("/index.html");
+		expect(response.status).toBe(200);
+	});
+
+	test(server1 + "/paulo.html should return 200", async () => {
+		const response = await request(server1)
+			.get("/paulo.html")
+		expect(response.status).toBe(200);
+	});
+
+	test(server1 + "/joao.html should return 200", async () => {
+		const response = await request(server1)
+			.get("/joao.html")
 		expect(response.status).toBe(200);
 	});
 
