54d10c0fa0063bb41607032104ab0e58d7b7254f
wleite <wleite@student.42sp.org.br>
2022-08-11 21:40:15 -0300

    test: ensure delete location works

---
+++ test/source/tests/delete.spec.js
@@ -0,0 +1,129 @@
+const request = require('supertest');
+const { v4: uuidv4 } = require('uuid');
+const fs = require('fs')
+const LoremIpsum = require("lorem-ipsum").LoremIpsum;
+
+function file_toString(path) {
+	var file = fs.readFileSync(path)
+		.toString('utf-8');
+	return file;
+}
+
+function create_file(content) {
+	var file = uuidv4();
+	fs.writeFileSync("/tmp/" + file, content);
+	return "/tmp/" + file;
+}
+
+function body_math(res_body, file) {
+
+	var body = create_file(res_body);
+	var ret = fs.readFileSync(body).equals(fs.readFileSync(file));
+	fs.unlinkSync(body);
+	return ret;
+}
+
+let server1 = "http://127.0.0.1:3490";
+let server_root = "../../server_root/";
+
+describe("DELETE", () => {
+	beforeAll(async () => {
+		const lorem = new LoremIpsum();
+		fs.writeFileSync("../www/delete/file1.txt", lorem.generateWords(400));
+		fs.writeFileSync("../www/delete/file2.txt", lorem.generateParagraphs(400));
+		fs.writeFileSync("../www/delete/cannot_delete", lorem.generateParagraphs(400));
+		fs.chmod("../www/delete/cannot_delete", 0o000, () => { });
+	});
+
+	afterAll(async () => {
+		fs.unlink("../www/delete/file1.txt", () => { });
+		fs.unlink("../www/delete/file2.txt", () => { });
+		fs.chmod("../www/delete/cannot_delete", 0o777, () => { });
+		fs.unlink("../www/delete/cannot_delete", () => { });
+	});
+
+	beforeEach(async () => {
+		await new Promise(r => setTimeout(() => r(), 10));
+	});
+
+	test(server1 + "/delete should return 204", async () => {
+		const res = await request(server1)
+			.delete("/delete");
+		expect(res.status).toBe(204);
+		expect(res.text).toBe("");
+	});
+
+	test(server1 + "/delete/ should return 204", async () => {
+		const res = await request(server1)
+			.delete("/delete/");
+		expect(res.status).toBe(204);
+		expect(res.text).toBe("");
+	});
+
+	test(server1 + "/delete/file1.txt should delete file1.txt", async () => {
+		const res = await request(server1)
+			.delete("/delete/file1.txt");
+		expect(res.status).toBe(204);
+		expect(fs.existsSync("../www/delete/file1.txt")).toBeFalsy();
+		expect(res.text).toBe("");
+	});
+
+	test(server1 + "/delete/file2.txt should delete file2.txt", async () => {
+		const res = await request(server1)
+			.delete("/delete/file2.txt");
+		expect(res.status).toBe(204);
+		expect(fs.existsSync("../www/delete/file2.txt")).toBeFalsy();
+		expect(res.text).toBe("");
+	});
+
+	test(server1 + "/delete/not_exists should return 404", async () => {
+		const res = await request(server1)
+			.delete("/delete/not_exists");
+		expect(res.status).toBe(404);
+		expect(fs.existsSync("../www/delete/not_exists")).toBeFalsy();
+		expect(body_math(res.text, server_root + "custom_404.html")).toBeTruthy();
+	});
+
+	test(server1 + "/delete/cannot_delete should return 401", async () => {
+		const res = await request(server1)
+			.delete("/delete/cannot_delete");
+		expect(res.status).toBe(401);
+		expect(fs.existsSync("../www/delete/cannot_delete")).toBeTruthy();
+		expect(res.text).toContain("401 Unauthorized");
+	});
+
+	test(server1 + "/ should return 405", async () => {
+		const res = await request(server1)
+			.delete("/");
+		expect(res.status).toBe(405);
+		expect(body_math(res.text, server_root + "custom_405.html")).toBeTruthy();
+	});
+
+	test(server1 + "/post should return 405", async () => {
+		const res = await request(server1)
+			.delete("/post");
+		expect(res.status).toBe(405);
+		expect(body_math(res.text, server_root + "custom_405.html")).toBeTruthy();
+	});
+
+	test(server1 + "/post/ should return 405", async () => {
+		const res = await request(server1)
+			.delete("/post/");
+		expect(res.status).toBe(405);
+		expect(body_math(res.text, server_root + "custom_405.html")).toBeTruthy();
+	});
+
+	test(server1 + "/put should return 405", async () => {
+		const res = await request(server1)
+			.delete("/put");
+		expect(res.status).toBe(405);
+		expect(body_math(res.text, server_root + "custom_405.html")).toBeTruthy();
+	});
+
+	test(server1 + "/put/ should return 405", async () => {
+		const res = await request(server1)
+			.delete("/put/");
+		expect(res.status).toBe(405);
+		expect(body_math(res.text, server_root + "custom_405.html")).toBeTruthy();
+	});
+});
