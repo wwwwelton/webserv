8e85f40fa0404ab681e84b0ba83f55d2a1dbbab9
wleite <wleite@student.42sp.org.br>
2022-08-17 21:14:26 -0300

    fix: sensitive case in header comparison

--- sources/request/Request.hpp
+++ sources/request/Request.hpp
@@ -7,13 +7,25 @@
 
 #pragma once
 #ifndef HTTP_REQUEST_HPP
-# define HTTP_REQUEST_HPP
+#define HTTP_REQUEST_HPP
 
+#include <algorithm>
 #include <iostream>
 #include <map>
+#include <string>
+
+struct Comparator {
+  bool operator()(const std::string& s1, const std::string& s2) const {
+    std::string str1(s1.length(), ' ');
+    std::string str2(s2.length(), ' ');
+    std::transform(s1.begin(), s1.end(), str1.begin(), tolower);
+    std::transform(s2.begin(), s2.end(), str2.begin(), tolower);
+    return str1 < str2;
+  }
+};
 
 struct Request {
-  std::map<std::string, std::string> headers;
+  std::map<std::string, std::string, Comparator> headers;
   std::string method;
   std::string path;
   std::string http_version;
@@ -25,9 +37,8 @@ struct Request {
 
   ~Request();
 
-  bool is_valid() const ;
-
+  bool is_valid() const;
 };
 
 std::ostream& operator<<(std::ostream& out, const Request& request);
-#endif // !HTTP_REQUEST_HPP
+#endif  // !HTTP_REQUEST_HPP
