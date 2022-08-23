ba18ca2897ea71022b4d6f4c68690a186cc0f7f9
wleite <wleite@student.42sp.org.br>
2022-08-23 20:33:26 -0300

    test: improve POST test description

--- test/source/tests/post.spec.js
+++ test/source/tests/post.spec.js
@@ -33,16 +33,16 @@ describe("POST", () => {
 		fs.unlink("../www/files_to_upload/500M.txt", () => { });
 		fs.unlink("../www/files_to_upload/600M.txt", () => { });
 
-		fs.unlink("../www/post/uploads/1W.txt", () => { });
-		fs.unlink("../www/post/uploads/1K.txt", () => { });
-		fs.unlink("../www/post/uploads/100K.txt", () => { });
-		fs.unlink("../www/post/uploads/1M.txt", () => { });
-		fs.unlink("../www/post/uploads/10M.txt", () => { });
-		fs.unlink("../www/post/uploads/20M.txt", () => { });
-		fs.unlink("../www/post/uploads/100M.txt", () => { });
-		fs.unlink("../www/post/uploads/101M.txt", () => { });
-		fs.unlink("../www/post/uploads/500M.txt", () => { });
-		fs.unlink("../www/post/uploads/600M.txt", () => { });
+		fs.unlink("../www/post_test/uploads/1W.txt", () => { });
+		fs.unlink("../www/post_test/uploads/1K.txt", () => { });
+		fs.unlink("../www/post_test/uploads/100K.txt", () => { });
+		fs.unlink("../www/post_test/uploads/1M.txt", () => { });
+		fs.unlink("../www/post_test/uploads/10M.txt", () => { });
+		fs.unlink("../www/post_test/uploads/20M.txt", () => { });
+		fs.unlink("../www/post_test/uploads/100M.txt", () => { });
+		fs.unlink("../www/post_test/uploads/101M.txt", () => { });
+		fs.unlink("../www/post_test/uploads/500M.txt", () => { });
+		fs.unlink("../www/post_test/uploads/600M.txt", () => { });
 
 		fs.unlink("../../php.ini", () => { });
 	});
@@ -53,162 +53,162 @@ describe("POST", () => {
 
 	jest.setTimeout(60000);
 
-	test(server1 + "/post2/ should return 200", async () => {
+	test(server1 + "/post_test2/ should return 200 because upload.php exists", async () => {
 		const res = await request(server1)
-			.post("/post2/")
-			.set("X-Webserv-Test", "/post2 should return 200");
+			.post("/post_test2/")
+			.set("X-Webserv-Test", "/post_test2/ should return 200 because upload.php exists");
 		expect(res.status).toBe(200);
 		expect(res.headers["content-type"]).toContain("text/html");
 		expect(res.headers["content-length"]).not.toBe("");
 		expect(res.text).not.toBe("");
 	});
 
-	test(server1 + "/post should return 301", async () => {
+	test(server1 + "/post_test should return 301 with Location: " + server1 + "/post_test/", async () => {
 		const res = await request(server1)
-			.post("/post")
-			.set("X-Webserv-Test", "/post should return 301");
+			.post("/post_test")
+			.set("X-Webserv-Test", "/post_test should return 301 with Location: " + server1 + "/post_test/");
 		expect(res.status).toBe(301);
 		expect(res.headers["content-type"]).toContain("text/html");
 		expect(res.headers["content-length"]).not.toBe("");
-		expect(res.headers["location"]).toBe(server1 + "/post/");
+		expect(res.headers["location"]).toBe(server1 + "/post_test/");
 	});
 
-	test(server1 + "/post2 should return 301", async () => {
+	test(server1 + "/post_test2 should return 301 with Location: " + server1 + "/post_test2/", async () => {
 		const res = await request(server1)
-			.post("/post2")
-			.set("X-Webserv-Test", "/post2 should return 301");
+			.post("/post_test2")
+			.set("X-Webserv-Test", "/post_test2 should return 301 with Location: " + server1 + "/post_test2/");
 		expect(res.status).toBe(301);
 		expect(res.headers["content-type"]).toContain("text/html");
 		expect(res.headers["content-length"]).not.toBe("");
-		expect(res.headers["location"]).toBe(server1 + "/post2/");
+		expect(res.headers["location"]).toBe(server1 + "/post_test2/");
 	});
 
-	test(server1 + "/post/ should return 403", async () => {
+	test(server1 + "/post_test/ should return 403 because index.php not exist", async () => {
 		const res = await request(server1)
-			.post("/post/")
-			.set("X-Webserv-Test", "/post/ should return 403");
+			.post("/post_test/")
+			.set("X-Webserv-Test", "/post_test/ should return 403 because index.php not exist");
 		expect(res.status).toBe(403);
 		expect(res.headers["content-type"]).toContain("text/html");
 		expect(res.headers["content-length"]).not.toBe("");
 	});
 
-	test(server1 + "/post/upload.php should receive a 1W file", async () => {
+	test(server1 + "/post_test/upload.php should receive a 1W file", async () => {
 		const res = await request(server1)
-			.post("/post/upload.php")
-			.set("X-Webserv-Test", "/post/upload.php should receive a 1W file")
+			.post("/post_test/upload.php")
+			.set("X-Webserv-Test", "/post_test/upload.php should receive a 1W file")
 			.attach("fileToUpload", "../www/files_to_upload/1W.txt");
 		expect(res.status).toBe(200);
 		expect(res.headers["content-type"]).toContain("text/html");
 		expect(res.headers["content-length"]).not.toBe("");
-		expect(fs.existsSync("../www/post/uploads/1W.txt")).toBeTruthy();
+		expect(fs.existsSync("../www/post_test/uploads/1W.txt")).toBeTruthy();
 		expect(res.text).toContain("1W.txt OK");
 	});
 
-	test(server1 + "/post/upload.php should receive a 1K file", async () => {
+	test(server1 + "/post_test/upload.php should receive a 1K file", async () => {
 		const res = await request(server1)
-			.post("/post/upload.php")
-			.set("X-Webserv-Test", "/post/upload.php should receive a 1K file")
+			.post("/post_test/upload.php")
+			.set("X-Webserv-Test", "/post_test/upload.php should receive a 1K file")
 			.attach("fileToUpload", "../www/files_to_upload/1K.txt");
 		expect(res.status).toBe(200);
 		expect(res.headers["content-type"]).toContain("text/html");
 		expect(res.headers["content-length"]).not.toBe("");
-		expect(fs.existsSync("../www/post/uploads/1K.txt")).toBeTruthy();
+		expect(fs.existsSync("../www/post_test/uploads/1K.txt")).toBeTruthy();
 		expect(res.text).toContain("1K.txt OK");
 	});
 
-	test(server1 + "/post/upload.php should receive a 100K file", async () => {
+	test(server1 + "/post_test/upload.php should receive a 100K file", async () => {
 		const res = await request(server1)
-			.post("/post/upload.php")
-			.set("X-Webserv-Test", "/post/upload.php should receive a 100K file")
+			.post("/post_test/upload.php")
+			.set("X-Webserv-Test", "/post_test/upload.php should receive a 100K file")
 			.attach("fileToUpload", "../www/files_to_upload/100K.txt");
 		expect(res.status).toBe(200);
 		expect(res.headers["content-type"]).toContain("text/html");
 		expect(res.headers["content-length"]).not.toBe("");
-		expect(fs.existsSync("../www/post/uploads/100K.txt")).toBeTruthy();
+		expect(fs.existsSync("../www/post_test/uploads/100K.txt")).toBeTruthy();
 		expect(res.text).toContain("100K.txt OK");
 	});
 
-	test(server1 + "/post/upload.php should receive a 1M file", async () => {
+	test(server1 + "/post_test/upload.php should receive a 1M file", async () => {
 		const res = await request(server1)
-			.post("/post/upload.php")
-			.set("X-Webserv-Test", "/post/upload.php should receive a 1M file")
+			.post("/post_test/upload.php")
+			.set("X-Webserv-Test", "/post_test/upload.php should receive a 1M file")
 			.attach("fileToUpload", "../www/files_to_upload/1M.txt");
 		expect(res.status).toBe(200);
 		expect(res.headers["content-type"]).toContain("text/html");
 		expect(res.headers["content-length"]).not.toBe("");
-		expect(fs.existsSync("../www/post/uploads/1M.txt")).toBeTruthy();
+		expect(fs.existsSync("../www/post_test/uploads/1M.txt")).toBeTruthy();
 		expect(res.text).toContain("1M.txt OK");
 	});
 
-	test(server1 + "/post/upload.php should receive a 10M file", async () => {
+	test(server1 + "/post_test/upload.php should receive a 10M file", async () => {
 		const res = await request(server1)
-			.post("/post/upload.php")
-			.set("X-Webserv-Test", "/post/upload.php should receive a 10M file")
+			.post("/post_test/upload.php")
+			.set("X-Webserv-Test", "/post_test/upload.php should receive a 10M file")
 			.attach("fileToUpload", "../www/files_to_upload/10M.txt");
 		expect(res.status).toBe(200);
 		expect(res.headers["content-type"]).toContain("text/html");
 		expect(res.headers["content-length"]).not.toBe("");
-		expect(fs.existsSync("../www/post/uploads/10M.txt")).toBeTruthy();
+		expect(fs.existsSync("../www/post_test/uploads/10M.txt")).toBeTruthy();
 		expect(res.text).toContain("10M.txt OK");
 	});
 
-	// test(server1 + "/post/upload.php should receive a 100M file", async () => {
+	// test(server1 + "/post_test/upload.php should receive a 100M file", async () => {
 	// 	const res = await request(server1)
-	// 		.post("/post/upload.php")
-	// 		.set("X-Webserv-Test", "/post/upload.php should receive a 100M file")
+	// 		.post("/post_test/upload.php")
+	// 		.set("X-Webserv-Test", "/post_test/upload.php should receive a 100M file")
 	// 		.attach("fileToUpload", "../www/files_to_upload/100M.txt");
 	// 	expect(res.status).toBe(200);
 	// 	expect(res.headers["content-type"]).toContain("text/html");
 	// 	expect(res.headers["content-length"]).not.toBe("");
-	// 	expect(fs.existsSync("../www/post/uploads/100M.txt")).toBeTruthy();
+	// 	expect(fs.existsSync("../www/post_test/uploads/100M.txt")).toBeTruthy();
 	// 	expect(res.text).toContain("100M.txt OK");
 	// });
 
-	// test(server1 + "/post/upload.php should receive a 101M file", async () => {
+	// test(server1 + "/post_test/upload.php should receive a 101M file", async () => {
 	// 	const res = await request(server1)
-	// 		.post("/post/upload.php")
-	// 		.set("X-Webserv-Test", "/post/upload.php should receive a 101M file")
+	// 		.post("/post_test/upload.php")
+	// 		.set("X-Webserv-Test", "/post_test/upload.php should receive a 101M file")
 	// 		.attach("fileToUpload", "../www/files_to_upload/101M.txt");
 	// 	expect(res.status).toBe(200);
 	// 	expect(res.headers["content-type"]).toContain("text/html");
 	// 	expect(res.headers["content-length"]).not.toBe("");
-	// 	expect(fs.existsSync("../www/post/uploads/101M.txt")).toBeTruthy();
+	// 	expect(fs.existsSync("../www/post_test/uploads/101M.txt")).toBeTruthy();
 	// 	expect(res.text).toContain("101M.txt OK");
 	// });
 
-	// test(server1 + "/post/upload.php should receive a 500M file", async () => {
+	// test(server1 + "/post_test/upload.php should receive a 500M file", async () => {
 	// 	const res = await request(server1)
-	// 		.post("/post/upload.php")
-	// 		.set("X-Webserv-Test", "/post/upload.php should receive a 500M file")
+	// 		.post("/post_test/upload.php")
+	// 		.set("X-Webserv-Test", "/post_test/upload.php should receive a 500M file")
 	// 		.attach("fileToUpload", "../www/files_to_upload/500M.txt");
 	// 	expect(res.status).toBe(200);
 	// 	expect(res.headers["content-type"]).toContain("text/html");
 	// 	expect(res.headers["content-length"]).not.toBe("");
-	// 	expect(fs.existsSync("../www/post/uploads/500M.txt")).toBeTruthy();
+	// 	expect(fs.existsSync("../www/post_test/uploads/500M.txt")).toBeTruthy();
 	// 	expect(res.text).toContain("500M.txt OK");
 	// });
 
-	// test(server1 + "/post/upload.php should not receive a 600M file", async () => {
+	// test(server1 + "/post_test/upload.php should not receive a 600M file", async () => {
 	// 	const res = await request(server1)
-	// 		.post("/post/upload.php")
-	// 		.set("X-Webserv-Test", "/post/upload.php should not receive a 600M file")
+	// 		.post("/post_test/upload.php")
+	// 		.set("X-Webserv-Test", "/post_test/upload.php should not receive a 600M file")
 	// 		.attach("fileToUpload", "../www/files_to_upload/600M.txt");
 	// 	expect(res.status).toBe(200);
 	// 	expect(res.headers["content-type"]).toContain("text/html");
 	// 	expect(res.headers["content-length"]).not.toBe("");
-	// 	expect(fs.existsSync("../www/post/uploads/600M.txt")).toBeFalsy();
+	// 	expect(fs.existsSync("../www/post_test/uploads/600M.txt")).toBeFalsy();
 	// 	expect(res.text).toContain("ERROR");
 	// });
 
-	test(server1 + "/post/upload.php should not receive a 20M file", async () => {
+	test(server1 + "/post_test/upload.php should not receive a 20M file", async () => {
 		const res = await request(server1)
-			.post("/post/upload.php")
-			.set("X-Webserv-Test", "/post/upload.php should not receive a 20M file")
+			.post("/post_test/upload.php")
+			.set("X-Webserv-Test", "/post_test/upload.php should not receive a 20M file")
 			.attach("fileToUpload", "../www/files_to_upload/20M.txt");
 		expect(res.status).toBe(200);
 		expect(res.headers["content-type"]).toContain("text/html");
 		expect(res.headers["content-length"]).not.toBe("");
-		expect(fs.existsSync("../www/post/uploads/20M.txt")).toBeFalsy();
+		expect(fs.existsSync("../www/post_test/uploads/20M.txt")).toBeFalsy();
 		expect(res.text).toContain("ERROR");
 	});
 });
